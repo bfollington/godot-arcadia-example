4f357564e82376ea834acd1e1c45691677d913cf
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-11-13 17:10:44 +1000

    State working with live-reload

--- bf.clj
+++ bf.clj
@@ -1,8 +1,8 @@
 (ns bf
   (:require [arcadia.core :as a]
             [arcadia.linear :refer [v2 v2+]]
-            [arcadia.2D :refer [position!]])
-  (:import [Godot Mathf]))
+            [arcadia.2D :refer [position! position]])
+  (:import [Godot GD Mathf]))
 
 (defn sin [t]
   (Mathf/Sin t))
@@ -11,12 +11,18 @@
   (Mathf/Cos t))
 
 (def elapsed (atom 0))
+(def base-position (atom (v2 0 0)))
 
-(defn foo-update [node key dt]
-  (a/log @elapsed)
+(defn do-update [node key dt]
   (swap! elapsed #(+ % dt))
-  (let [offset (v2 256 256)
-        x (* 32 (sin @elapsed))
-        y (* 32 (cos @elapsed))
-        new-pos (v2+ (v2 x y) offset)]
+  (let [origin (a/state node :initial-pos)
+        t (* @elapsed (a/state node :speed))
+        x (* 267 (* (cos (* 2 t)) (sin t)))
+        y (* 128 (sin t))
+        new-pos (v2+ (v2 x y) origin)]
     (position! node new-pos)))
+
+(defn ready [node key]
+  (a/log "Setting initial state...")
+  (a/set-state node :initial-pos (position node))
+  (a/set-state node :speed (GD/RandRange 0.5 2)))
